bb6eab801315d5d77fceb019745ce9f45dae85f0
yueyingjuna <1046396106@qq.com>
2018-04-02 17:13:11 +0800

    page

--- src/components/selectStu.vue
+++ src/components/selectStu.vue
@@ -4,12 +4,14 @@
         <router-link to="/addSignStu" tag="button" type="button" class="el-button el-button--primary el-icon-plus">单条添加</router-link>
 
         <router-link to="/addMoreStu" tag="button" type="button" class="el-button el-button--primary el-icon-more">批量添加</router-link>
+
+
     <el-table
             :data="tableData"
             style="width: 100%">
         <el-table-column
-                prop="date"
-                label="日期"
+                prop="id"
+                label="id"
                 width="180">
         </el-table-column>
         <el-table-column
@@ -18,10 +20,13 @@
                 width="180">
         </el-table-column>
         <el-table-column
-                prop="address"
-                label="地址">
+                prop="cname"
+                label="班级">
         </el-table-column>
     </el-table>
+
+        <el-button type="info" @click="up()">上一下</el-button>
+        <el-button type="info" @click="next()">下一页</el-button>
     </div>
 </template>
 
@@ -30,8 +35,49 @@
         name: "select-stu",
         data() {
             return {
-                tableData: []
+                tableData: [],
+                page:0,
+                pages:0
             }
+        },
+        methods:{
+            up(){
+                if(this.page>0) {
+                    this.page-=1
+                    fetch("/api/stu/select?page=" + this.page).then(function (e) {
+                        return e.json();
+                    }).then((e) => {
+                        this.tableData = e;
+                    })
+                }
+            },
+            next(){
+
+                console.log(this.pages);
+                if(this.page<this.pages-1) {
+                    this.page+=1;
+                    fetch("/api/stu/select?page=" + this.page).then(function (e) {
+                        return e.json();
+                    }).then((e) => {
+                        this.tableData = e;
+                    })
+                }
+
+            }
+
+        },
+        mounted(){
+            fetch("/api/stu/selectAll").then(function (e) {
+                return e.text();
+            }).then((e)=>{
+                this.pages=e;
+            })
+
+            fetch("/api/stu/select?page="+this.page).then(function (e) {
+                return e.json();
+            }).then((e)=>{
+                this.tableData=e;
+            })
         }
     }
 </script>
--- src/components/stu/addMoreStu.vue
+++ src/components/stu/addMoreStu.vue
@@ -3,7 +3,9 @@
             name="aa"
             class="upload-demo"
             action="/api/stu/upload"
-            :file-list="fileList">
+            :file-list="fileList"
+            :on-success="success"
+    >
         <el-button size="small" type="primary">点击上传</el-button>
         <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
     </el-upload>
@@ -29,6 +31,9 @@
             },
             beforeRemove(file, fileList) {
                 return this.$confirm(`确定移除 ${ file.name }？`);
+            },
+            success(e){
+                console.log(e);
             }
         }
     }
